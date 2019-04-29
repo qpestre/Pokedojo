4e6773dfa5cd861e585a948fd4faf0568781778d
qpestre <quentin.pestre.sorge@gmail.com>
2019-04-29 21:40:35 +0200

    Création de l'interface, classe MondeDesPokemons, selection des 3 pokemons

--- Pokedojo/MondeDesPokemons.cs
+++ Pokedojo/MondeDesPokemons.cs
@@ -6,6 +6,31 @@ namespace Pokedojo
 {
     class MondeDesPokemons
     {
+        //Attribut(s)
+        List<Pokemon> _listePokemons; //Notre liste des 151 pokemons
+        
+        //Accesseur
+        public List<Pokemon> ListePokemons
+        {
+            get;
+            set;
+        }
 
+        //Constructeur
+        public MondeDesPokemons(List<Pokemon> listePokemons)
+        {
+            _listePokemons = listePokemons;
+        }
+        //Surdéfinition
+        public MondeDesPokemons() : this(new List<Pokemon>())
+        {
+        }
+
+        //Fonction pour ajouter les pokemons dans _listePokemons du MondeDesPokemons 
+        //A utiliser dans le Program main dans la boucle de lecture du fichier.csv pour referencer tous les pokemons
+        public void ReferencerPokemon(Pokemon pokemon)
+        {
+            _listePokemons.Add(pokemon);
+        }
     }
 }
--- Pokedojo/Program.cs
+++ Pokedojo/Program.cs
@@ -7,48 +7,119 @@ namespace Pokedojo
     {
         static void Main(string[] args)
         {
+            Interface InterfaceActive = new Interface();
+            InterfaceActive.Accueil();
+            Console.WriteLine(InterfaceActive.NomJoueur);
+            Console.ReadLine();
 
-        //INTERFACE
 
-        //Lire la BDD.csv
-        //stocker dans des listes 
-        //Déclarer tous les pokemons
 
-        //MondeDesPokemons
-        //Dresseurs
-        //Tournoi
+            //INTERFACE
+
+            //Lire la BDD.csv
+            //stocker dans des listes 
+            //Déclarer tous les pokemons
+
+            //MondeDesPokemons
+            //Dresseurs
+            //Tournoi
             //Combat
+            //Affrontement
+            //using System;
+
+namespace Pokedojo
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //TEST ACCUEIL
+                /*
+                Interface InterfaceActive = new Interface();
+                InterfaceActive.Accueil();
+                Console.WriteLine(InterfaceActive.NomJoueur);
+                Console.ReadLine();
+                */
+
+                //TEST CHOIX PKMN
+                /*
+                Evolution0 Pkmn1 = new Evolution0("Salameche", 40, 34, "feu", "eau", 0, false, "ok", 20, 4);
+                Evolution0 Pkmn2 = new Evolution0("Aspicot", 40, 34, "feu", "eau", 0, true, "ok", 20, 13);
+                Evolution0 Pkmn3 = new Evolution0("Mew", 40, 34, "feu", "eau", 0, false, "ko", 20, 151);
+                Interface InterfaceActive = new Interface();
+                InterfaceActive.ChoixPkmn(Pkmn1, Pkmn2, Pkmn3);
+                Console.WriteLine(InterfaceActive.PkmnChoisit._index);
+                Console.ReadLine();
+                */
+
+                //INTERFACE
+
+                //Lire la BDD.csv
+                //stocker dans des listes 
+                //Déclarer tous les pokemons
+
+                //MondeDesPokemons
+                //Dresseurs
+                //Tournoi
+                //Combat
                 //Affrontement
-           //
-        //RESULTAT TOURNOI
+                //
+                //RESULTAT TOURNOI
 
 
 
-        // Fonction pour lire .csv à adapter
+                // Fonction pour lire .csv à adapter
 
-        //    public static List<string> lectureCSV()
-        //    {
-        //        using (var reader = new StreamReader(@"C:\test.csv"))
-        //        {
-        //            List<string> listA = new List<string>();
-        //            List<string> listB = new List<string>();
+                //    public static List<string> lectureCSV()
+                //    {
+                //        using (var reader = new StreamReader(@"C:\test.csv"))
+                //        {
+                //            List<string> listA = new List<string>();
+                //            List<string> listB = new List<string>();
 
-        //            while (!reader.EndOfStream)
-        //            {
-        //                var line = reader.ReadLine();
-        //                var values = line.Split(';');
+                //            while (!reader.EndOfStream)
+                //            {
+                //                var line = reader.ReadLine();
+                //                var values = line.Split(';');
 
-        //                listA.Add(values[0]);
-        //                listB.Add(values[1]);
-        //            }
-        //        }
-        //    }
+                //                listA.Add(values[0]);
+                //                listB.Add(values[1]);
+                //            }
+                //        }
+                //    }
+            }
         }
+    }
 
-        
+    //RESULTAT TOURNOI
 
 
-        
+
+    // Fonction pour lire .csv à adapter
+
+    //    public static List<string> lectureCSV()
+    //    {
+    //        using (var reader = new StreamReader(@"C:\test.csv"))
+    //        {
+    //            List<string> listA = new List<string>();
+    //            List<string> listB = new List<string>();
+
+    //            while (!reader.EndOfStream)
+    //            {
+    //                var line = reader.ReadLine();
+    //                var values = line.Split(';');
+
+    //                listA.Add(values[0]);
+    //                listB.Add(values[1]);
+    //            }
+    //        }
+    //    }
+}
+
+
+
+
+
     }
 
 
