80a0ea12f28b93861d60c1366767c263f04b966a
qpestre <quentin.pestre.sorge@gmail.com>
2019-04-29 11:30:43 +0200

    Architecture du programme en commentaires

--- Pokedojo/Program.cs
+++ Pokedojo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Pokedojo
 {
@@ -6,7 +7,49 @@ namespace Pokedojo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        //INTERFACE
+
+        //Lire la BDD.csv
+        //stocker dans des listes 
+        //Déclarer tous les pokemons
+
+        //MondeDesPokemons
+        //Dresseurs
+        //Tournoi
+            //Combat
+                //Affrontement
+           //
+        //RESULTAT TOURNOI
+
+
+
+        // Fonction pour lire .csv à adapter
+
+        //    public static List<string> lectureCSV()
+        //    {
+        //        using (var reader = new StreamReader(@"C:\test.csv"))
+        //        {
+        //            List<string> listA = new List<string>();
+        //            List<string> listB = new List<string>();
+
+        //            while (!reader.EndOfStream)
+        //            {
+        //                var line = reader.ReadLine();
+        //                var values = line.Split(';');
+
+        //                listA.Add(values[0]);
+        //                listB.Add(values[1]);
+        //            }
+        //        }
+        //    }
         }
+
+        
+
+
+        
     }
+
+
 }
